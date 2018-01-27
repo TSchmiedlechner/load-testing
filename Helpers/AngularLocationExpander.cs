de4f1c49d1606ab8b18287ba35a8787214351dca
Tom Schmiedlechner <toms@sbg.at>
2018-01-28 00:52:29 +0100

    Added blank project

---
+++ Helpers/AngularLocationExpander.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+
+namespace LoadTesting.Helpers
+{
+  public class AngularLocationExpander : IViewLocationExpander
+  {
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+      return new List<string>
+      {
+        "~/wwwroot/app/views/{1}/{0}.html",
+        "~/wwwroot/app/{0}.html"
+      };
+    }
+
+    public void PopulateValues(ViewLocationExpanderContext context) { }
+  }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using LoadTesting.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace LoadTesting
+{
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().AddJsonOptions(opt =>
+      {
+        opt.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+      });
+      services.AddOptions();
+      services.Configure<RazorViewEngineOptions>(o =>
+      {
+        o.ViewLocationExpanders.Add(new AngularLocationExpander());
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+
+      app.Use(async (context, next) =>
+      {
+        await next();
+        if (context.Response.StatusCode == 404 &&
+            !Path.HasExtension(context.Request.Path.Value) &&
+            !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next();
+        }
+      });
+      app.UseMvcWithDefaultRoute();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+    }
+  }
+}
